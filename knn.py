9a101ce823fe22ba6afd0437ec6351c97321233c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-09 20:56:07 +0200

    fixes

--- knn.py
+++ knn.py
@@ -3,3 +3,11 @@ from parser import getLearnData, getTestData
 
 LEARN_DATA = getLearnData()
 TEST_DATA = getTestData()
+
+EU = []
+CB = []
+M3 = []
+
+for learnItem in LEARN_DATA:
+    for testItem in TEST_DATA:
+        print()
