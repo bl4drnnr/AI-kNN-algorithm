53234e91e5c6e0d371804c0d9a03c4185e392a31
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-09 21:45:58 +0200

    sorted cb and m3

--- knn.py
+++ knn.py
@@ -19,6 +19,8 @@ for testItem in TEST_DATA:
              })
     # Then, I need to sort this data by type
     sortedEU = sorted(DATA_WITH_METRICS, key=lambda x: x['EU'], reverse=True)
+    sortedCB = sorted(DATA_WITH_METRICS, key=lambda x: x['CB'], reverse=True)
+    sortedM3 = sorted(DATA_WITH_METRICS, key=lambda x: x['M3'], reverse=True)
     DATA_WITH_METRICS = []
     # Get 1/3/5 from top
     # And push it to EU, CB, M3
