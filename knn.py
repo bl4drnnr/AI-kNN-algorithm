1eef215aa79c16b5995f4de7b454d4982255bf07
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-10 18:35:55 +0200

    reverse fix

--- knn.py
+++ knn.py
@@ -18,23 +18,23 @@ for testItem in TEST_DATA:
              'M3': Minkowski(learnItem, testItem)
              })
     # Then, I need to sort this data by type
-    sortedEU = sorted(DATA_WITH_METRICS, key=lambda x: x['EU'], reverse=True)
-    sortedCB = sorted(DATA_WITH_METRICS, key=lambda y: y['CB'], reverse=True)
-    sortedM3 = sorted(DATA_WITH_METRICS, key=lambda z: z['M3'], reverse=True)
+    sortedEU = sorted(DATA_WITH_METRICS, key=lambda x: x['EU'], reverse=False)
+    sortedCB = sorted(DATA_WITH_METRICS, key=lambda y: y['CB'], reverse=False)
+    sortedM3 = sorted(DATA_WITH_METRICS, key=lambda z: z['M3'], reverse=False)
     # Get 1/3/5 from top
     for i in range(1, 6, 2):
-        normalElectionEU = normalElection(sortedEU)
-        distanceSumElectionEU = distanceSumElection(sortedEU)
-        sumOfReciprocalEU = sumOfReciprocalOfTheSquaresOfDistances(sortedEU)
+        normalElectionEU = normalElection(sortedEU[:i])
+        distanceSumElectionEU = distanceSumElection(sortedEU[:i])
+        sumOfReciprocalEU = sumOfReciprocalOfTheSquaresOfDistances(sortedEU[:i])
 
-        normalElectionCB = normalElection(sortedCB)
-        distanceSumElectionCB = distanceSumElection(sortedCB)
-        sumOfReciprocalCB = sumOfReciprocalOfTheSquaresOfDistances(sortedCB)
+        normalElectionCB = normalElection(sortedCB[:i])
+        distanceSumElectionCB = distanceSumElection(sortedCB[:i])
+        sumOfReciprocalCB = sumOfReciprocalOfTheSquaresOfDistances(sortedCB[:i])
 
-        normalElectionM3 = normalElection(sortedM3)
-        distanceSumElectionM3 = distanceSumElection(sortedM3)
-        sumOfReciprocalM3 = sumOfReciprocalOfTheSquaresOfDistances(sortedM3)
-        
+        normalElectionM3 = normalElection(sortedM3[:i])
+        distanceSumElectionM3 = distanceSumElection(sortedM3[:i])
+        sumOfReciprocalM3 = sumOfReciprocalOfTheSquaresOfDistances(sortedM3[:i])
+
     DATA_WITH_METRICS = []
 
     # And push it to EU, CB, M3
